d4a4c2fd210b6d21281e1f47388d23532cb9bd12
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-09-11 21:29:27 +0600

    Create an Spring Framework DI Example project

---
+++ src/main/java/kz/zhelezyaka/didemo/DiDemoApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiDemoApplication.class, args);
+    }
+
+}
