3f27b37f8388f6907e4feb79da28c9691974b548
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-19 22:06:52 +0600

    Using Spring Qualifier Annotations

--- src/main/java/kz/zhelezyaka/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/kz/zhelezyaka/didemo/controllers/ConstructorInjectedController.java
@@ -1,15 +1,14 @@
 package kz.zhelezyaka.didemo.controllers;
 
 import kz.zhelezyaka.didemo.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructorInjectedController {
     private GreetingService greetingService;
 
-    @Autowired
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
---
+++ src/main/java/kz/zhelezyaka/didemo/services/GetterGreetingService.java
@@ -0,0 +1,11 @@
+package kz.zhelezyaka.didemo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetterGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hello - I was injected by the getter";
+    }
+}
