34ef08d050caaa85f2e69e6a43bc109240ff1026
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-23 15:35:09 +0600

    Spring Bean life cycle demo

---
+++ src/main/java/kz/zhelezyaka/didemo/CustomBeanPostProcessor.java
@@ -0,0 +1,23 @@
+package kz.zhelezyaka.didemo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof LifeCycleDemoBean) {
+            ((LifeCycleDemoBean) bean).beforeInit();
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof LifeCycleDemoBean) {
+            ((LifeCycleDemoBean) bean).afterInit();
+        }
+        return bean;
+    }
+}
