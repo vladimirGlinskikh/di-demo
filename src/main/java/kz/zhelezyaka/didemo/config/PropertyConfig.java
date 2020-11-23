4396de40729973cc0a838a34c3e71fa9aa35f274
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-11-23 07:02:14 +0600

    Property source in action for example

---
+++ src/main/java/kz/zhelezyaka/didemo/config/PropertyConfig.java
@@ -0,0 +1,36 @@
+package kz.zhelezyaka.didemo.config;
+
+import kz.zhelezyaka.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+    @Value("${kz.username}")
+    String user;
+
+    @Value("${kz.password}")
+    String password;
+
+    @Value("${kz.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
